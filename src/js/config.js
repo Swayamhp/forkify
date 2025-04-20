35605b6e9bbea8324999b38adf6b53d768fe8b0d
Swyamhp <swayamgd0@gmail.com>
2025-04-20 10:44:08 +0530

    updatedKey

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'ee09e170-23f1-4983-835f-a6c447b3a395';
 export const MODAL_CLOSE_SEC = 2.5;
